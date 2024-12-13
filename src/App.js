001e2663806af806fa724b68660536ad786bfc7d
Ivana Carrillo <ivanae.carrillo@gmail.com>
2024-12-12 21:02:35 -0500

    added links to navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Home from "./pages/Home.js";
+import Contact_Us from "./pages/Contact Us.js";
+import Services from "./Pages/Services.js";
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <div className="navbar">
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/About Us">About Us</Link>
+            </li>
+            <li>
+              <Link to="/services">Services</Link>
+            </li>
+          </ul>
+        </div>
+        <h1>You Found the G Spot</h1>
+      </div>
+    </Router>
   );
 }
-
-export default App;
